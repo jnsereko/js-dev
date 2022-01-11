dd59c5b19c8d5f05c52ddb826376a060d428824a
jnsereko <nserekojowashi@gmail.com>
2022-01-11 14:15:47 +0300

    Cleaned final Protorype

---
+++ add.js
@@ -0,0 +1,850 @@
+[
+    {
+     "name": "Dillauti [1]",
+     "people": [
+      {
+       "name": "Winifred Graham [1]",
+       "animals": [
+        {
+         "name": "Anoa"
+        },
+        {
+         "name": "Duck"
+        },
+        {
+         "name": "Narwhal"
+        },
+        {
+         "name": "Badger"
+        },
+        {
+         "name": "Cobra"
+        },
+        {
+         "name": "Crow"
+        }
+       ]
+      },
+      {
+       "name": "Blanche Viciani [2]",
+       "animals": [
+        {
+         "name": "Barbet"
+        },
+        {
+         "name": "Rhea"
+        },
+        {
+         "name": "Snakes"
+        },
+        {
+         "name": "Antelope"
+        },
+        {
+         "name": "Echidna"
+        },
+        {
+         "name": "Crow"
+        },
+        {
+         "name": "Guinea Fowl"
+        },
+        {
+         "name": "Deer Mouse"
+        }
+       ]
+      },
+      {
+       "name": "Philip Murray [3]",
+       "animals": [
+        {
+         "name": "Sand Dollar"
+        },
+        {
+         "name": "Buzzard"
+        },
+        {
+         "name": "Elephant"
+        },
+        {
+         "name": "Xenops"
+        },
+        {
+         "name": "Dormouse"
+        },
+        {
+         "name": "Anchovy"
+        },
+        {
+         "name": "Dinosaur"
+        }
+       ]
+      },
+      {
+       "name": "Bobby Ristori [4]",
+       "animals": [
+        {
+         "name": "Kowari"
+        },
+        {
+         "name": "Caecilian"
+        },
+        {
+         "name": "Common Genet"
+        },
+        {
+         "name": "Chipmunk"
+        },
+        {
+         "name": "Aardwolf"
+        },
+        {
+         "name": "Przewalski's Horse"
+        },
+        {
+         "name": "Badger"
+        },
+        {
+         "name": "Sand Cat"
+        },
+        {
+         "name": "Linne's Two-toed Sloth"
+        }
+       ]
+      },
+      {
+       "name": "Louise Pinzauti [5]",
+       "animals": [
+        {
+         "name": "Manta Ray"
+        },
+        {
+         "name": "Nubian Ibex"
+        },
+        {
+         "name": "Warbler"
+        },
+        {
+         "name": "Duck"
+        },
+        {
+         "name": "Mice"
+        }
+       ]
+      }
+     ]
+    },
+    {
+     "name": "Tohabdal [2]",
+     "people": [
+      {
+       "name": "Effie Houghton [6]",
+       "animals": [
+        {
+         "name": "Zebra"
+        },
+        {
+         "name": "Ring-tailed Lemur"
+        },
+        {
+         "name": "Fly"
+        },
+        {
+         "name": "Blue Iguana"
+        },
+        {
+         "name": "Emu"
+        },
+        {
+         "name": "African Wild Ass"
+        },
+        {
+         "name": "Numbat"
+        }
+       ]
+      },
+      {
+       "name": "Essie Bennett [7]",
+       "animals": [
+        {
+         "name": "Aldabra Tortoise"
+        },
+        {
+         "name": "Patagonian Toothfish"
+        },
+        {
+         "name": "Giant Panda"
+        },
+        {
+         "name": "Goat"
+        },
+        {
+         "name": "Quahog"
+        },
+        {
+         "name": "Collared Lemur"
+        },
+        {
+         "name": "Aldabra Tortoise"
+        }
+       ]
+      },
+      {
+       "name": "Owen Bongini [8]",
+       "animals": [
+        {
+         "name": "Zebrashark"
+        },
+        {
+         "name": "Dogs"
+        },
+        {
+         "name": "Mouse"
+        },
+        {
+         "name": "Numbat"
+        },
+        {
+         "name": "African Wild Dog"
+        }
+       ]
+      },
+      {
+       "name": "Alexander Fleury [9]",
+       "animals": [
+        {
+         "name": "Gelada"
+        },
+        {
+         "name": "Rattlesnake"
+        },
+        {
+         "name": "Rabbit"
+        },
+        {
+         "name": "Aardvark"
+        },
+        {
+         "name": "Duck"
+        },
+        {
+         "name": "Courser"
+        },
+        {
+         "name": "Woodpecker"
+        }
+       ]
+      },
+      {
+       "name": "Curtis Fuchs [10]",
+       "animals": [
+        {
+         "name": "Squirrel"
+        },
+        {
+         "name": "Falcon"
+        },
+        {
+         "name": "Cat"
+        },
+        {
+         "name": "Lobe Coral"
+        },
+        {
+         "name": "Camel"
+        },
+        {
+         "name": "Broadclub Cuttlefish"
+        }
+       ]
+      },
+      {
+       "name": "Maud Lorenzo [11]",
+       "animals": [
+        {
+         "name": "Bush Dog"
+        },
+        {
+         "name": "Sea Urchin"
+        },
+        {
+         "name": "Gayal"
+        },
+        {
+         "name": "Tortoise"
+        },
+        {
+         "name": "Meerkat"
+        },
+        {
+         "name": "Lion"
+        },
+        {
+         "name": "Gecko"
+        }
+       ]
+      },
+      {
+       "name": "Linnie Lamb [12]",
+       "animals": [
+        {
+         "name": "Burro"
+        },
+        {
+         "name": "African Wild Dog"
+        },
+        {
+         "name": "Slender Snipe Eel"
+        },
+        {
+         "name": "Red Panda"
+        },
+        {
+         "name": "Baby Doll Sheep"
+        },
+        {
+         "name": "California Sea Lion"
+        },
+        {
+         "name": "Rabbits"
+        }
+       ]
+      },
+      {
+       "name": "Randall Benoît [13]",
+       "animals": [
+        {
+         "name": "Chameleons"
+        },
+        {
+         "name": "Bee-eater"
+        },
+        {
+         "name": "King Vulture"
+        },
+        {
+         "name": "Giant Isopod"
+        },
+        {
+         "name": "Sand Cat"
+        }
+       ]
+      }
+     ]
+    },
+    {
+     "name": "Uzuzozne [3]",
+     "people": [
+      {
+       "name": "Harold Patton [14]",
+       "animals": [
+        {
+         "name": "Bearded Dragon"
+        },
+        {
+         "name": "Chicken"
+        },
+        {
+         "name": "Sand Cat"
+        },
+        {
+         "name": "Hedgehog"
+        },
+        {
+         "name": "Collared Lemur"
+        },
+        {
+         "name": "Frogmouth"
+        },
+        {
+         "name": "Raccoon dog"
+        },
+        {
+         "name": "Shortfin Mako Shark"
+        }
+       ]
+      },
+      {
+       "name": "Millie Lapini [15]",
+       "animals": [
+        {
+         "name": "Bearded Dragon"
+        },
+        {
+         "name": "Peafowl"
+        },
+        {
+         "name": "Aardvark"
+        },
+        {
+         "name": "Cows"
+        },
+        {
+         "name": "Crane Fly"
+        },
+        {
+         "name": "Rock Hyrax"
+        },
+        {
+         "name": "Gerbils"
+        },
+        {
+         "name": "Brown Bear"
+        }
+       ]
+      },
+      {
+       "name": "Lillian Calamandrei [16]",
+       "animals": [
+        {
+         "name": "Rats"
+        },
+        {
+         "name": "Macaw"
+        },
+        {
+         "name": "Gazelle"
+        },
+        {
+         "name": "Gazelle"
+        },
+        {
+         "name": "Alpaca"
+        },
+        {
+         "name": "Snakes"
+        },
+        {
+         "name": "Yellowjacket"
+        },
+        {
+         "name": "Stickleback"
+        }
+       ]
+      },
+      {
+       "name": "Lina Allen [17]",
+       "animals": [
+        {
+         "name": "Rabbit"
+        },
+        {
+         "name": "Cats"
+        },
+        {
+         "name": "Jaguarundi"
+        },
+        {
+         "name": "Duck"
+        },
+        {
+         "name": "Caribbean Flamingo"
+        },
+        {
+         "name": "Oyster"
+        },
+        {
+         "name": "Agouti"
+        }
+       ]
+      },
+      {
+       "name": "Georgia Hooper [18]",
+       "animals": [
+        {
+         "name": "Grasshopper"
+        },
+        {
+         "name": "Polar Bear"
+        },
+        {
+         "name": "Rabbit"
+        },
+        {
+         "name": "Loggerhead Turtle"
+        },
+        {
+         "name": "Rhinoceros"
+        },
+        {
+         "name": "African Wild Dog"
+        },
+        {
+         "name": "Jackal"
+        },
+        {
+         "name": "Zebu"
+        }
+       ]
+      },
+      {
+       "name": "Lillie Abbott [19]",
+       "animals": [
+        {
+         "name": "John Dory"
+        },
+        {
+         "name": "Gayal"
+        },
+        {
+         "name": "Hawk"
+        },
+        {
+         "name": "Umbrella Squid"
+        },
+        {
+         "name": "Hyrax"
+        },
+        {
+         "name": "Henkel's Leaf-tailed Gecko"
+        }
+       ]
+      },
+      {
+       "name": "Philip Davis [20]",
+       "animals": [
+        {
+         "name": "Mini Donkey"
+        },
+        {
+         "name": "Flatback Turtle"
+        },
+        {
+         "name": "Rabbit"
+        },
+        {
+         "name": "Zebra"
+        },
+        {
+         "name": "Rhea"
+        },
+        {
+         "name": "Leafy Seadragon"
+        },
+        {
+         "name": "Bat"
+        },
+        {
+         "name": "Caterpillar"
+        }
+       ]
+      }
+     ]
+    },
+    {
+     "name": "Zuhackog [4]",
+     "people": [
+      {
+       "name": "Elva Baroni [21]",
+       "animals": [
+        {
+         "name": "Silkworm"
+        },
+        {
+         "name": "Zebu"
+        },
+        {
+         "name": "King Vulture"
+        },
+        {
+         "name": "Zebrashark"
+        },
+        {
+         "name": "Ostrich"
+        },
+        {
+         "name": "Waxwing"
+        }
+       ]
+      },
+      {
+       "name": "Johnny Graziani [22]",
+       "animals": [
+        {
+         "name": "Dunnart"
+        },
+        {
+         "name": "Cotinga"
+        },
+        {
+         "name": "Carp"
+        },
+        {
+         "name": "Bat"
+        },
+        {
+         "name": "Olive Sea Snake"
+        },
+        {
+         "name": "Caterpillar"
+        },
+        {
+         "name": "Jackal"
+        }
+       ]
+      },
+      {
+       "name": "Herman Christensen [23]",
+       "animals": [
+        {
+         "name": "Death Adder"
+        },
+        {
+         "name": "Pronghorn"
+        },
+        {
+         "name": "Carp"
+        },
+        {
+         "name": "Jaguar"
+        },
+        {
+         "name": "Anteater"
+        },
+        {
+         "name": "Zebu"
+        },
+        {
+         "name": "Red Ruffed Lemur"
+        }
+       ]
+      },
+      {
+       "name": "Fannie Ancillotti [24]",
+       "animals": [
+        {
+         "name": "Silkworm"
+        },
+        {
+         "name": "Horses"
+        },
+        {
+         "name": "Anaconda"
+        },
+        {
+         "name": "Guinea"
+        },
+        {
+         "name": "Bird"
+        },
+        {
+         "name": "Aardwolf"
+        },
+        {
+         "name": "Crane Fly"
+        },
+        {
+         "name": "Caterpillar"
+        }
+       ]
+      },
+      {
+       "name": "Lawrence Camiciottoli [25]",
+       "animals": [
+        {
+         "name": "Bustard"
+        },
+        {
+         "name": "Numbat"
+        },
+        {
+         "name": "Cat"
+        },
+        {
+         "name": "Gecko"
+        },
+        {
+         "name": "Northern Red Snapper"
+        },
+        {
+         "name": "Monkfish"
+        },
+        {
+         "name": "Birds"
+        },
+        {
+         "name": "Caterpillar"
+        },
+        {
+         "name": "Mule"
+        }
+       ]
+      },
+      {
+       "name": "Marion Landi [26]",
+       "animals": [
+        {
+         "name": "Tortoise"
+        },
+        {
+         "name": "Mule"
+        },
+        {
+         "name": "Hedgehog"
+        },
+        {
+         "name": "Geckos"
+        },
+        {
+         "name": "Sheep"
+        },
+        {
+         "name": "Emu"
+        }
+       ]
+      },
+      {
+       "name": "Lou de Bruin [27]",
+       "animals": [
+        {
+         "name": "Boa"
+        },
+        {
+         "name": "Death Adder"
+        },
+        {
+         "name": "Okapi"
+        },
+        {
+         "name": "Fly"
+        },
+        {
+         "name": "Horses"
+        }
+       ]
+      }
+     ]
+    },
+    {
+     "name": "Satanwi [5]",
+     "people": [
+      {
+       "name": "Elmer Kinoshita [28]",
+       "animals": [
+        {
+         "name": "Weasel"
+        },
+        {
+         "name": "Birds"
+        },
+        {
+         "name": "Snakes"
+        },
+        {
+         "name": "Anteater"
+        },
+        {
+         "name": "Groundhog"
+        },
+        {
+         "name": "Ant"
+        },
+        {
+         "name": "Courser"
+        }
+       ]
+      },
+      {
+       "name": "Cora Howell [29]",
+       "animals": [
+        {
+         "name": "Rhea"
+        },
+        {
+         "name": "Sponge"
+        },
+        {
+         "name": "Cat"
+        },
+        {
+         "name": "African Wild Dog"
+        },
+        {
+         "name": "Snakes"
+        },
+        {
+         "name": "Starling"
+        },
+        {
+         "name": "Pronghorn"
+        }
+       ]
+      },
+      {
+       "name": "Ernest Conte [30]",
+       "animals": [
+        {
+         "name": "Bird"
+        },
+        {
+         "name": "Colugo"
+        },
+        {
+         "name": "Grison"
+        },
+        {
+         "name": "Pot Bellied Pig"
+        },
+        {
+         "name": "Asian Elephant"
+        }
+       ]
+      },
+      {
+       "name": "Dennis Franci [31]",
+       "animals": [
+        {
+         "name": "Grouse"
+        },
+        {
+         "name": "Hapuka"
+        },
+        {
+         "name": "Cheetah"
+        },
+        {
+         "name": "Donkey"
+        },
+        {
+         "name": "Turkey"
+        },
+        {
+         "name": "Carp"
+        },
+        {
+         "name": "Octopus"
+        },
+        {
+         "name": "Silkworm"
+        },
+        {
+         "name": "Bearded Dragon"
+        }
+       ]
+      },
+      {
+       "name": "Anthony Bruno [32]",
+       "animals": [
+        {
+         "name": "Caracal"
+        },
+        {
+         "name": "Anteater"
+        },
+        {
+         "name": "Kiwa Hirsuta"
+        },
+        {
+         "name": "Zooplankton"
+        },
+        {
+         "name": "Tarantula"
+        },
+        {
+         "name": "Oryx"
+        }
+       ]
+      }
+     ]
+    }
+   ]
